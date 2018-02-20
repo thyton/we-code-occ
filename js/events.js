6a295e0ef91bad15b434120ea388870cd40e71da
Thy Ton <maithytonn@gmail.com>
2018-02-20 00:08:54 -0800

    Calendar was off 1 day. Fixed that and added buttons to navigate schedule

--- js/events.js
+++ js/events.js
@@ -1,41 +1,63 @@
 let events = {"2018-02-20": [{"time": "12:50PM - 2:50PM", "location": "MBCC 134"}, {"time": "5:50PM - 7:50PM", "location": "MBCC 136"}], "2018-02-21": [{"time": "12:00PM - 3:40PM", "location": "MBCC 136"}], "2018-02-22": [{"time": "12:50PM - 2:50PM", "location": "MBCC 134"}, {"time": "2:50PM - 5:50PM", "location": "MBCC 137"}, {"time": "5:50PM - 7:50PM", "location": "MBCC 125"}], "2018-02-26": [{"time": "10:40AM - 12:40PM", "location": "MBCC 125"}, {"time": "1:40PM - 3:40PM", "location": "MBCC 125"}], "2018-02-27": [{"time": "12:50PM - 2:50PM", "location": "MBCC 134"}, {"time": "5:50PM - 7:50PM", "location": "MBCC 136"}], "2018-02-28": [{"time": "12:00PM - 3:40PM", "location": "MBCC 136"}], "2018-03-01": [{"time": "12:50PM - 2:50PM", "location": "MBCC 134"}, {"time": "2:50PM - 5:50PM", "location": "MBCC 137"}, {"time": "5:50PM - 7:50PM", "location": "MBCC 125"}], "2018-03-05": [{"time": "10:40AM - 12:40PM", "location": "MBCC 125"}, {"time": "1:40PM - 3:40PM", "location": "MBCC 125"}], "2018-03-06": [{"time": "12:50PM - 2:50PM", "location": "MBCC 134"}, {"time": "5:50PM - 7:50PM", "location": "MBCC 136"}], "2018-03-07": [{"time": "12:00PM - 3:40PM", "location": "MBCC 136"}], "2018-03-12": [{"time": "10:40AM - 12:40PM", "location": "MBCC 125"}, {"time": "1:40PM - 3:40PM", "location": "MBCC 125"}], "2018-03-13": [{"time": "12:50PM - 2:50PM", "location": "MBCC 134"}, {"time": "5:50PM - 7:50PM", "location": "MBCC 136"}], "2018-03-19": [{"time": "10:40AM - 12:40PM", "location": "MBCC 125"}, {"time": "1:40PM - 3:40PM", "location": "MBCC 125"}], "2018-03-22": [{"time": "12:50PM - 2:50PM", "location": "MBCC 134"}, {"time": "2:50PM - 5:50PM", "location": "MBCC 137"}, {"time": "5:50PM - 7:50PM", "location": "MBCC 125"}], "2018-03-28": [{"time": "12:00PM - 3:40PM", "location": "MBCC 136"}], "2018-03-29": [{"time": "12:50PM - 2:50PM", "location": "MBCC 134"}, {"time": "5:50PM - 7:50PM", "location": "MBCC 125"}], "2018-04-03": [{"time": "12:50PM - 2:50PM", "location": "MBCC 134"}, {"time": "2:00PM - 5:00PM", "location": "MBCC 138"}, {"time": "5:50PM - 7:50PM", "location": "MBCC 136"}], "2018-04-04": [{"time": "12:00PM - 3:40PM", "location": "MBCC 136"}], "2018-04-05": [{"time": "12:50PM - 2:50PM", "location": "MBCC 134"}, {"time": "5:50PM - 7:50PM", "location": "MBCC 125"}], "2018-04-09": [{"time": "10:40AM - 12:40PM", "location": "MBCC 125"}, {"time": "1:40PM - 3:40PM", "location": "MBCC 125"}], "2018-04-10": [{"time": "12:50PM - 2:50PM", "location": "MBCC 134"}, {"time": "2:00PM - 5:00PM", "location": "MBCC 138"}, {"time": "5:50PM - 7:50PM", "location": "MBCC 136"}], "2018-04-11": [{"time": "12:00PM - 3:40PM", "location": "MBCC 136"}], "2018-04-12": [{"time": "12:50PM - 2:50PM", "location": "MBCC 134"}, {"time": "5:50PM - 7:50PM", "location": "MBCC 125"}], "2018-04-16": [{"time": "10:40AM - 12:40PM", "location": "MBCC 125"}, {"time": "1:40PM - 3:40PM", "location": "MBCC 125"}], "2018-04-17": [{"time": "12:50PM - 2:50PM", "location": "MBCC 134"}, {"time": "2:00PM - 5:00PM", "location": "MBCC 138"}, {"time": "5:50PM - 7:50PM", "location": "MBCC 136"}], "2018-04-18": [{"time": "12:00PM - 3:40PM", "location": "MBCC 136"}], "2018-04-23": [{"time": "10:40AM - 12:40PM", "location": "MBCC 125"}, {"time": "1:40PM - 3:40PM", "location": "MBCC 125"}], "2018-04-24": [{"time": "12:50PM - 2:50PM", "location": "MBCC 134"}, {"time": "2:00PM - 5:00PM", "location": "MBCC 138"}, {"time": "5:50PM - 7:50PM", "location": "MBCC 136"}], "2018-04-30": [{"time": "10:40AM - 12:40PM", "location": "MBCC 125"}, {"time": "1:40PM - 3:40PM", "location": "MBCC 125"}], "2018-05-03": [{"time": "12:50PM - 2:50PM", "location": "MBCC 134"}, {"time": "5:50PM - 7:50PM", "location": "MBCC 125"}], "2018-05-09": [{"time": "12:00PM - 3:40PM", "location": "MBCC 136"}], "2018-05-10": [{"time": "12:50PM - 2:50PM", "location": "MBCC 134"}, {"time": "5:50PM - 7:50PM", "location": "MBCC 125"}], "2018-05-15": [{"time": "12:50PM - 2:50PM", "location": "MBCC 134"}, {"time": "2:00PM - 5:00PM", "location": "MBCC 138"}, {"time": "5:50PM - 7:50PM", "location": "MBCC 136"}], "2018-05-16": [{"time": "12:00PM - 3:40PM", "location": "MBCC 136"}], "2018-05-17": [{"time": "12:50PM - 2:50PM", "location": "MBCC 134"}, {"time": "5:50PM - 7:50PM", "location": "MBCC 125"}]};
 
-
-function getMondayOfCurrentWeek(d)
+const minDate = new Date('2018-02-25T23:59:59');
+let current = new Date();
+function showBackScheduleButton()
 {
-    let day = d.getDay();
-    return new Date(d.getFullYear(), d.getMonth(), d.getDate() + (day == 0?-6:1)-day );
+  if(current >= minDate)
+  {
+    document.getElementById("back-schedule").disable = (current >= minDate?false:true);
+  }
 }
-
-function yymmdd(day){
-  return day.toISOString().substring(0,10);
-}
-
-today = new Date();
-today.setDate(today.getDate() + 12);
-monday = getMondayOfCurrentWeek(today);
-let thisWeek = { 'mon': '', 'tue': '', 'wed':'', 'thu': ''};
-let count = 0;
-let meetingsHTML = {};
-let date = '';
-let calculatedDay = new Date();
-for (var day in thisWeek)
+function getMondayOfCurrentWeek()
 {
-  meetingsHTML[day] = ''
-  calculatedDay.setDate(monday.getDate() + count);
-  thisWeek[day]= yymmdd(calculatedDay);
-  date += '<td>' + thisWeek[day].substring(5).replace('-',' / ') + "</td>";
-
-  var meetings = events[thisWeek[day]];
-  for (var meeting in meetings)
-  {
-    meetingsHTML[day] += '<li class=time>' + meetings[meeting].time + "</li>";
-    meetingsHTML[day] += '<li class=location>' + meetings[meeting].location + "</li>";
-  }
-  if(meetingsHTML[day] != '')
-  {
-    document.getElementById(day).innerHTML= meetingsHTML[day];
-  }
-  ++count;
+  day = current.getDay()
+  return new Date(current.getFullYear(), current.getMonth(),
+                  current.getDate() + (day == 0?-6:1)-day );
 }
-document.getElementById("date").innerHTML = date;
+
+function yymmdd(d){
+  return d.toISOString().substring(0,10);
+}
+
+function getWeekSchedule()
+{
+  let calculatedDay = getMondayOfCurrentWeek();
+  let week = { 'mon': '', 'tue': '', 'wed':'', 'thu': ''};
+  let meetingsHTML = {};
+  let date = '';
+  for (var day in week)
+  {
+    meetingsHTML[day] = ''
+    week[day]= yymmdd(calculatedDay);
+    date += '<td>' + week[day].substring(5).replace('-',' / ') + "</td>";
+    var meetings = events[week[day]];
+    for (var meeting in meetings)
+    {
+      meetingsHTML[day] += '<li><span class=time>'
+                            + meetings[meeting].time + "</span></li>";
+
+      meetingsHTML[day] += '<li class=location>'
+                            + meetings[meeting].location + "</li>";
+    }
+    if(meetingsHTML[day] != '')
+    {
+      document.getElementById(day).innerHTML= meetingsHTML[day];
+    }
+    calculatedDay.setDate(calculatedDay.getDate() + 1);
+  }
+  document.getElementById("date").innerHTML = date;
+}
+
+function getNextWeekSchedule()
+{
+  current.setDate(current.getDate() + 7);
+  getWeekSchedule();
+}
+
+function getLastWeekSchedule()
+{
+  current.setDate(current.getDate() - 7);
+  getWeekSchedule();
+  showBackScheduleButton();
+}
