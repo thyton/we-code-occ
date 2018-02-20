f2e82940ea93ebb38c0128f9a08f7082144ffc16
Thy Ton <maithytonn@gmail.com>
2018-02-20 12:39:34 -0800

    Fixed the data of schedule

--- events.py
+++ events.py
@@ -11,13 +11,14 @@ lastMon = day + timedelta(days=84)
 
 while day <= lastMon:
     for count in range(0,4):
-        day += timedelta(days=1)
         dayStr = str(day.date())
-        if day.weekday() == 0:
-            events[dayStr] = [{'time': '10:40AM - 12:40PM','location': "MBCC 125"},
-                             {'time': '1:40PM - 3:40PM','location': "MBCC 125"}]
+        if day.weekday() == 0 and day > datetime(2018,2,19):
+            events[dayStr] = [{'time': '1:40PM - 3:40PM','location': "MBCC 125"}]
+
         elif day.weekday() == 1:
-            events[dayStr] = [{'time': '12:50PM - 2:50PM','location': "MBCC 134"}]
+            events[dayStr] = []
+            if day >= datetime(2018,2,20):
+                events[dayStr].append({'time': '12:50PM - 2:50PM','location': "MBCC 134"})
             if day >= datetime(2018,4,3):
                 events[dayStr].append({'time': '2:00PM - 5:00PM','location': "MBCC 138"})
             events[dayStr].append({'time': '5:50PM - 7:50PM','location': "MBCC 136"})
@@ -26,12 +27,16 @@ while day <= lastMon:
             events[dayStr] = [{'time': '12:00PM - 3:40PM','location': "MBCC 136"}]
 
         elif day.weekday() == 3:
-            events[dayStr] = [{'time': '12:50PM - 2:50PM','location': "MBCC 134"}]
+            events[dayStr] = []
+            if day > datetime(2018,2,22):
+                events[dayStr].append({'time': '12:50PM - 2:50PM','location': "MBCC 134"})
             if day.weekday() == datetime(2018,3,8):
                 events[dayStr].append({'time': '2:50PM - 5:50PM','location': "MBCC 138"})
             elif day <= datetime(2018,3,22):
                 events[dayStr].append({'time': '2:50PM - 5:50PM','location': "MBCC 137"})
             events[dayStr].append({'time': '5:50PM - 7:50PM','location': "MBCC 125"})
-    day += timedelta(days=2)
+        day += timedelta(days=1)
+
+    day += timedelta(days=3)
 
 print(json.dumps(events))
--- js/events.js
+++ js/events.js
@@ -1,13 +1,10 @@
-let events = {"2018-02-20": [{"time": "12:50PM - 2:50PM", "location": "MBCC 134"}, {"time": "5:50PM - 7:50PM", "location": "MBCC 136"}], "2018-02-21": [{"time": "12:00PM - 3:40PM", "location": "MBCC 136"}], "2018-02-22": [{"time": "12:50PM - 2:50PM", "location": "MBCC 134"}, {"time": "2:50PM - 5:50PM", "location": "MBCC 137"}, {"time": "5:50PM - 7:50PM", "location": "MBCC 125"}], "2018-02-26": [{"time": "10:40AM - 12:40PM", "location": "MBCC 125"}, {"time": "1:40PM - 3:40PM", "location": "MBCC 125"}], "2018-02-27": [{"time": "12:50PM - 2:50PM", "location": "MBCC 134"}, {"time": "5:50PM - 7:50PM", "location": "MBCC 136"}], "2018-02-28": [{"time": "12:00PM - 3:40PM", "location": "MBCC 136"}], "2018-03-01": [{"time": "12:50PM - 2:50PM", "location": "MBCC 134"}, {"time": "2:50PM - 5:50PM", "location": "MBCC 137"}, {"time": "5:50PM - 7:50PM", "location": "MBCC 125"}], "2018-03-05": [{"time": "10:40AM - 12:40PM", "location": "MBCC 125"}, {"time": "1:40PM - 3:40PM", "location": "MBCC 125"}], "2018-03-06": [{"time": "12:50PM - 2:50PM", "location": "MBCC 134"}, {"time": "5:50PM - 7:50PM", "location": "MBCC 136"}], "2018-03-07": [{"time": "12:00PM - 3:40PM", "location": "MBCC 136"}], "2018-03-12": [{"time": "10:40AM - 12:40PM", "location": "MBCC 125"}, {"time": "1:40PM - 3:40PM", "location": "MBCC 125"}], "2018-03-13": [{"time": "12:50PM - 2:50PM", "location": "MBCC 134"}, {"time": "5:50PM - 7:50PM", "location": "MBCC 136"}], "2018-03-19": [{"time": "10:40AM - 12:40PM", "location": "MBCC 125"}, {"time": "1:40PM - 3:40PM", "location": "MBCC 125"}], "2018-03-22": [{"time": "12:50PM - 2:50PM", "location": "MBCC 134"}, {"time": "2:50PM - 5:50PM", "location": "MBCC 137"}, {"time": "5:50PM - 7:50PM", "location": "MBCC 125"}], "2018-03-28": [{"time": "12:00PM - 3:40PM", "location": "MBCC 136"}], "2018-03-29": [{"time": "12:50PM - 2:50PM", "location": "MBCC 134"}, {"time": "5:50PM - 7:50PM", "location": "MBCC 125"}], "2018-04-03": [{"time": "12:50PM - 2:50PM", "location": "MBCC 134"}, {"time": "2:00PM - 5:00PM", "location": "MBCC 138"}, {"time": "5:50PM - 7:50PM", "location": "MBCC 136"}], "2018-04-04": [{"time": "12:00PM - 3:40PM", "location": "MBCC 136"}], "2018-04-05": [{"time": "12:50PM - 2:50PM", "location": "MBCC 134"}, {"time": "5:50PM - 7:50PM", "location": "MBCC 125"}], "2018-04-09": [{"time": "10:40AM - 12:40PM", "location": "MBCC 125"}, {"time": "1:40PM - 3:40PM", "location": "MBCC 125"}], "2018-04-10": [{"time": "12:50PM - 2:50PM", "location": "MBCC 134"}, {"time": "2:00PM - 5:00PM", "location": "MBCC 138"}, {"time": "5:50PM - 7:50PM", "location": "MBCC 136"}], "2018-04-11": [{"time": "12:00PM - 3:40PM", "location": "MBCC 136"}], "2018-04-12": [{"time": "12:50PM - 2:50PM", "location": "MBCC 134"}, {"time": "5:50PM - 7:50PM", "location": "MBCC 125"}], "2018-04-16": [{"time": "10:40AM - 12:40PM", "location": "MBCC 125"}, {"time": "1:40PM - 3:40PM", "location": "MBCC 125"}], "2018-04-17": [{"time": "12:50PM - 2:50PM", "location": "MBCC 134"}, {"time": "2:00PM - 5:00PM", "location": "MBCC 138"}, {"time": "5:50PM - 7:50PM", "location": "MBCC 136"}], "2018-04-18": [{"time": "12:00PM - 3:40PM", "location": "MBCC 136"}], "2018-04-23": [{"time": "10:40AM - 12:40PM", "location": "MBCC 125"}, {"time": "1:40PM - 3:40PM", "location": "MBCC 125"}], "2018-04-24": [{"time": "12:50PM - 2:50PM", "location": "MBCC 134"}, {"time": "2:00PM - 5:00PM", "location": "MBCC 138"}, {"time": "5:50PM - 7:50PM", "location": "MBCC 136"}], "2018-04-30": [{"time": "10:40AM - 12:40PM", "location": "MBCC 125"}, {"time": "1:40PM - 3:40PM", "location": "MBCC 125"}], "2018-05-03": [{"time": "12:50PM - 2:50PM", "location": "MBCC 134"}, {"time": "5:50PM - 7:50PM", "location": "MBCC 125"}], "2018-05-09": [{"time": "12:00PM - 3:40PM", "location": "MBCC 136"}], "2018-05-10": [{"time": "12:50PM - 2:50PM", "location": "MBCC 134"}, {"time": "5:50PM - 7:50PM", "location": "MBCC 125"}], "2018-05-15": [{"time": "12:50PM - 2:50PM", "location": "MBCC 134"}, {"time": "2:00PM - 5:00PM", "location": "MBCC 138"}, {"time": "5:50PM - 7:50PM", "location": "MBCC 136"}], "2018-05-16": [{"time": "12:00PM - 3:40PM", "location": "MBCC 136"}], "2018-05-17": [{"time": "12:50PM - 2:50PM", "location": "MBCC 134"}, {"time": "5:50PM - 7:50PM", "location": "MBCC 125"}]};
+let events = {"2018-02-20": [{"time": "12:50PM - 2:50PM", "location": "MBCC 134"}, {"time": "5:50PM - 7:50PM", "location": "MBCC 136"}], "2018-02-21": [{"time": "12:00PM - 3:40PM", "location": "MBCC 136"}], "2018-02-22": [{"time": "2:50PM - 5:50PM", "location": "MBCC 137"}, {"time": "5:50PM - 7:50PM", "location": "MBCC 125"}], "2018-02-26": [{"time": "1:40PM - 3:40PM", "location": "MBCC 125"}], "2018-02-27": [{"time": "12:50PM - 2:50PM", "location": "MBCC 134"}, {"time": "5:50PM - 7:50PM", "location": "MBCC 136"}], "2018-02-28": [{"time": "12:00PM - 3:40PM", "location": "MBCC 136"}], "2018-03-01": [{"time": "12:50PM - 2:50PM", "location": "MBCC 134"}, {"time": "2:50PM - 5:50PM", "location": "MBCC 137"}, {"time": "5:50PM - 7:50PM", "location": "MBCC 125"}], "2018-03-05": [{"time": "1:40PM - 3:40PM", "location": "MBCC 125"}], "2018-03-06": [{"time": "12:50PM - 2:50PM", "location": "MBCC 134"}, {"time": "5:50PM - 7:50PM", "location": "MBCC 136"}], "2018-03-07": [{"time": "12:00PM - 3:40PM", "location": "MBCC 136"}], "2018-03-08": [{"time": "12:50PM - 2:50PM", "location": "MBCC 134"}, {"time": "2:50PM - 5:50PM", "location": "MBCC 137"}, {"time": "5:50PM - 7:50PM", "location": "MBCC 125"}], "2018-03-12": [{"time": "1:40PM - 3:40PM", "location": "MBCC 125"}], "2018-03-13": [{"time": "12:50PM - 2:50PM", "location": "MBCC 134"}, {"time": "5:50PM - 7:50PM", "location": "MBCC 136"}], "2018-03-14": [{"time": "12:00PM - 3:40PM", "location": "MBCC 136"}], "2018-03-15": [{"time": "12:50PM - 2:50PM", "location": "MBCC 134"}, {"time": "2:50PM - 5:50PM", "location": "MBCC 137"}, {"time": "5:50PM - 7:50PM", "location": "MBCC 125"}], "2018-03-19": [{"time": "1:40PM - 3:40PM", "location": "MBCC 125"}], "2018-03-20": [{"time": "12:50PM - 2:50PM", "location": "MBCC 134"}, {"time": "5:50PM - 7:50PM", "location": "MBCC 136"}], "2018-03-21": [{"time": "12:00PM - 3:40PM", "location": "MBCC 136"}], "2018-03-22": [{"time": "12:50PM - 2:50PM", "location": "MBCC 134"}, {"time": "2:50PM - 5:50PM", "location": "MBCC 137"}, {"time": "5:50PM - 7:50PM", "location": "MBCC 125"}], "2018-03-26": [{"time": "1:40PM - 3:40PM", "location": "MBCC 125"}], "2018-03-27": [{"time": "12:50PM - 2:50PM", "location": "MBCC 134"}, {"time": "5:50PM - 7:50PM", "location": "MBCC 136"}], "2018-03-28": [{"time": "12:00PM - 3:40PM", "location": "MBCC 136"}], "2018-03-29": [{"time": "12:50PM - 2:50PM", "location": "MBCC 134"}, {"time": "5:50PM - 7:50PM", "location": "MBCC 125"}], "2018-04-02": [{"time": "1:40PM - 3:40PM", "location": "MBCC 125"}], "2018-04-03": [{"time": "12:50PM - 2:50PM", "location": "MBCC 134"}, {"time": "2:00PM - 5:00PM", "location": "MBCC 138"}, {"time": "5:50PM - 7:50PM", "location": "MBCC 136"}], "2018-04-04": [{"time": "12:00PM - 3:40PM", "location": "MBCC 136"}], "2018-04-05": [{"time": "12:50PM - 2:50PM", "location": "MBCC 134"}, {"time": "5:50PM - 7:50PM", "location": "MBCC 125"}], "2018-04-09": [{"time": "1:40PM - 3:40PM", "location": "MBCC 125"}], "2018-04-10": [{"time": "12:50PM - 2:50PM", "location": "MBCC 134"}, {"time": "2:00PM - 5:00PM", "location": "MBCC 138"}, {"time": "5:50PM - 7:50PM", "location": "MBCC 136"}], "2018-04-11": [{"time": "12:00PM - 3:40PM", "location": "MBCC 136"}], "2018-04-12": [{"time": "12:50PM - 2:50PM", "location": "MBCC 134"}, {"time": "5:50PM - 7:50PM", "location": "MBCC 125"}], "2018-04-16": [{"time": "1:40PM - 3:40PM", "location": "MBCC 125"}], "2018-04-17": [{"time": "12:50PM - 2:50PM", "location": "MBCC 134"}, {"time": "2:00PM - 5:00PM", "location": "MBCC 138"}, {"time": "5:50PM - 7:50PM", "location": "MBCC 136"}], "2018-04-18": [{"time": "12:00PM - 3:40PM", "location": "MBCC 136"}], "2018-04-19": [{"time": "12:50PM - 2:50PM", "location": "MBCC 134"}, {"time": "5:50PM - 7:50PM", "location": "MBCC 125"}], "2018-04-23": [{"time": "1:40PM - 3:40PM", "location": "MBCC 125"}], "2018-04-24": [{"time": "12:50PM - 2:50PM", "location": "MBCC 134"}, {"time": "2:00PM - 5:00PM", "location": "MBCC 138"}, {"time": "5:50PM - 7:50PM", "location": "MBCC 136"}], "2018-04-25": [{"time": "12:00PM - 3:40PM", "location": "MBCC 136"}], "2018-04-26": [{"time": "12:50PM - 2:50PM", "location": "MBCC 134"}, {"time": "5:50PM - 7:50PM", "location": "MBCC 125"}], "2018-04-30": [{"time": "1:40PM - 3:40PM", "location": "MBCC 125"}], "2018-05-01": [{"time": "12:50PM - 2:50PM", "location": "MBCC 134"}, {"time": "2:00PM - 5:00PM", "location": "MBCC 138"}, {"time": "5:50PM - 7:50PM", "location": "MBCC 136"}], "2018-05-02": [{"time": "12:00PM - 3:40PM", "location": "MBCC 136"}], "2018-05-03": [{"time": "12:50PM - 2:50PM", "location": "MBCC 134"}, {"time": "5:50PM - 7:50PM", "location": "MBCC 125"}], "2018-05-07": [{"time": "1:40PM - 3:40PM", "location": "MBCC 125"}], "2018-05-08": [{"time": "12:50PM - 2:50PM", "location": "MBCC 134"}, {"time": "2:00PM - 5:00PM", "location": "MBCC 138"}, {"time": "5:50PM - 7:50PM", "location": "MBCC 136"}], "2018-05-09": [{"time": "12:00PM - 3:40PM", "location": "MBCC 136"}], "2018-05-10": [{"time": "12:50PM - 2:50PM", "location": "MBCC 134"}, {"time": "5:50PM - 7:50PM", "location": "MBCC 125"}], "2018-05-14": [{"time": "1:40PM - 3:40PM", "location": "MBCC 125"}], "2018-05-15": [{"time": "12:50PM - 2:50PM", "location": "MBCC 134"}, {"time": "2:00PM - 5:00PM", "location": "MBCC 138"}, {"time": "5:50PM - 7:50PM", "location": "MBCC 136"}], "2018-05-16": [{"time": "12:00PM - 3:40PM", "location": "MBCC 136"}], "2018-05-17": [{"time": "12:50PM - 2:50PM", "location": "MBCC 134"}, {"time": "5:50PM - 7:50PM", "location": "MBCC 125"}]};
 
-const minDate = new Date('2018-02-25T23:59:59');
+const minDate = new Date('February 25, 2018 23:59:00');
 let current = new Date();
 function showBackScheduleButton()
 {
-  if(current >= minDate)
-  {
-    document.getElementById("back-schedule").disable = (current >= minDate?false:true);
-  }
+    document.getElementById("back-schedule").disable = (current > minDate?false:true);
 }
 function getMondayOfCurrentWeek()
 {
@@ -32,17 +29,21 @@ function getWeekSchedule()
     week[day]= yymmdd(calculatedDay);
     date += '<td>' + week[day].substring(5).replace('-',' / ') + "</td>";
     var meetings = events[week[day]];
-    for (var meeting in meetings)
+    if(meetings != [])
     {
-      meetingsHTML[day] += '<li><span class=time>'
-                            + meetings[meeting].time + "</span></li>";
+      for (var meeting in meetings)
+      {
+        meetingsHTML[day] += '<li><span class=time>'
+                              + meetings[meeting].time + "</span></li>";
 
-      meetingsHTML[day] += '<li class=location>'
-                            + meetings[meeting].location + "</li>";
-    }
-    if(meetingsHTML[day] != '')
-    {
+        meetingsHTML[day] += '<li class=location>'
+                              + meetings[meeting].location + "</li>";
+      }
       document.getElementById(day).innerHTML= meetingsHTML[day];
+    }
+    else
+    {
+      document.getElementById(day).innerHTML='';
     }
     calculatedDay.setDate(calculatedDay.getDate() + 1);
   }
@@ -52,12 +53,20 @@ function getWeekSchedule()
 function getNextWeekSchedule()
 {
   current.setDate(current.getDate() + 7);
+  document.getElementById("back-schedule").disable = false;
   getWeekSchedule();
 }
 
 function getLastWeekSchedule()
 {
-  current.setDate(current.getDate() - 7);
-  getWeekSchedule();
-  showBackScheduleButton();
+  if (current < minDate)
+  {
+    document.getElementById("back-schedule").disable = true;
+  }
+  else
+  {
+    document.getElementById("back-schedule").disable = false;
+    current.setDate(current.getDate() - 7);
+    getWeekSchedule();
+  }
 }
